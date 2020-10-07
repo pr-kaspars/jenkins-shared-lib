532e5d62c48b41591a1e5d1a11f1b1efb74ad5ec
Kaspars Primaks <kaspars.primaks@gmail.com>
2020-10-07 21:53:12 +0100

    helm signature

--- src/com/github/prkaspars/jenkins/HelmCommandFactory.groovy
+++ src/com/github/prkaspars/jenkins/HelmCommandFactory.groovy
@@ -2,13 +2,13 @@ package com.github.prkaspars.jenkins
 
 class HelmCommandFactory {
 
-    static String upgrade(String release, String chart, String[] flags) {
+    static String upgrade(String release, String chart, Iterable<String> flags) {
         List<String> cmd = ["helm", "upgrade", release, chart]
         cmd.addAll(flags)
         return cmd.join(" ")
     }
 
-    static String lint(String path, String[] flags) {
+    static String lint(String path, Iterable<String> flags) {
         List<String> cmd = ["helm", "lint", path]
         cmd.addAll(flags)
         return cmd.join(" ")
