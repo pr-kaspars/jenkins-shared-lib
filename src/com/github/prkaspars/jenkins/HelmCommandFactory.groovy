3b61e3cbd3d54a6871f2b3bdb8eec39eb44e8105
Kaspars Primaks <kaspars.primaks@gmail.com>
2020-10-07 22:07:10 +0100

    revert

--- src/com/github/prkaspars/jenkins/HelmCommandFactory.groovy
+++ src/com/github/prkaspars/jenkins/HelmCommandFactory.groovy
@@ -3,10 +3,14 @@ package com.github.prkaspars.jenkins
 class HelmCommandFactory {
 
     static String upgrade(String release, String chart, Iterable<String> flags) {
-        return (["helm", "upgrade", release, chart] + flags).join(" ")
+        List<String> cmd = ['helm', 'upgrade', release, chart]
+        cmd.addAll(flags)
+        return cmd.join(' ')
     }
 
     static String lint(String path, Iterable<String> flags) {
-        return (["helm", "lint", path] + flags).join(' ')
+        List<String> cmd = ['helm', 'lint', path]
+        cmd.addAll(flags)
+        return cmd.join(' ')
     }
 }
